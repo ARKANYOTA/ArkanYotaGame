fa78b3a5ff7189d799e852b86f480992d244d688
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2020-06-07 21:40:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'ArkanYotaGame',         # How you named your package folder (MyLib)
   packages = ['ArkanYotaGame'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Game in Cmd',   # Give a short description about your library
   author = 'ARKANYOTA',                   # Type in your name
